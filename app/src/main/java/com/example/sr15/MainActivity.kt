9d1127a7bee53136b94d3b43aa048aa86de063b3
pawelglaba <64959534+pawelglaba@users.noreply.github.com>
2021-05-19 16:44:29 +0200

    first Classes

--- app/src/main/java/com/example/sr15/MainActivity.kt
+++ app/src/main/java/com/example/sr15/MainActivity.kt
@@ -1,11 +1,64 @@
 package com.example.sr15
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+
+    private var userInput: EditText? = null
+    private var button: Button? = null
+    private var textView: TextView? = null
+    private var nextViewButton: Button? = null
+
+    private var numTimesClicked = 0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        userInput = findViewById(R.id.userInput)
+        button = findViewById(R.id.button)
+        nextViewButton = findViewById(R.id.nextView)
+
+
+        nextViewButton?.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(v: View?) {
+                openActivity2()
+            }
+        })
+
+
+
+        textView = findViewById(R.id.printText)
+        textView?.text=""
+        textView?.movementMethod=ScrollingMovementMethod();
+
+
+        button?.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(v: View?) {
+              numTimesClicked +=1
+                textView?.append("\n The button got tapped by ${userInput?.text} $numTimesClicked time")
+                if(numTimesClicked !=1){
+                    textView?.append("s")
+                }
+            }
+        })
+
+
     }
+
+    private fun openActivity2(){
+        val intent = Intent(this, secondActivity::class.java)
+        startActivity(intent)
+    }
+
+
 }
