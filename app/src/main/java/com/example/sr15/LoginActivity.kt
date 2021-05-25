96402af6fe6107842032098ee34caebb600458ea
pawelglaba <64959534+pawelglaba@users.noreply.github.com>
2021-05-25 12:56:21 +0200

    LoginActivity

--- app/src/main/java/com/example/sr15/LoginActivity.kt
+++ app/src/main/java/com/example/sr15/LoginActivity.kt
@@ -1,11 +1,20 @@
 package com.example.sr15
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+
+
+    fun goToRegister(view: View) {
+        val intent = Intent(this, RegisterActivity::class.java)
+        startActivity(intent)
     }
 }
